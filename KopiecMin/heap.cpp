d8efc346074ae585a8f807c6c4afbd92a6610efa
Piotr Ogrodnik <piotrekogrodnik81@gmail.com>
2017-03-13 17:55:22 +0100

    Operator+ implementation.

--- KopiecMin/heap.cpp
+++ KopiecMin/heap.cpp
@@ -30,16 +30,21 @@ heap::~heap() {
 int heap::operator+(int val) {
 	numberOfNodes++;
 	int k = numberOfNodes;
-	node *auxptr;	//creating new pointer pointing at a node
-	auxptr = new node[k];	//creating a new array of nodes
-	if (k != 1) {
-		memcpy(auxptr, nodeArray, sizeof(nodeArray));	//if that's not the first node we make,
-		delete[] nodeArray;	//cpy the memory of the old array to the new, relasing the old one
+	//node *auxptr;	//creating new pointer pointing at a node
+	//auxptr = new node[k];	//creating a new array of nodes
+	if (k == 1) {
+		//memcpy(auxptr, nodeArray, sizeof(nodeArray));	//if that's not the first node we make,
+		//delete[] nodeArray;	//cpy the memory of the old array to the new, relasing the old one
+		nodeArray = new node[2];
 		
 	}
-	nodeArray = auxptr;
+	else {
+		nodeArray = static_cast<node*> (realloc(nodeArray, sizeof(node)*(k+1) ) ); //casting the returned void to node ptr
+
+	}
+	//nodeArray = auxptr;
 	nodeArray[k].value = val;
-	if (parentNmbr(k) == 0) { //for first node
+	if (k == 1) { //for first node
 		nodeArray[k].parent = 0;
 		nodeArray[k].leftChild = 0;
 		nodeArray[k].rightChild = 0;
@@ -48,7 +53,7 @@ int heap::operator+(int val) {
 		nodeArray[k].parent = &nodeArray[parentNmbr(k)];
 		nodeArray[k].leftChild = 0;
 		nodeArray[k].rightChild = 0;
-		if (nodeArray[k].parent->leftChild) {
+		if (!nodeArray[k].parent->leftChild) {
 			nodeArray[k].parent->leftChild = &nodeArray[k];
 		}
 		else {
@@ -57,13 +62,16 @@ int heap::operator+(int val) {
 	}
 	//beginning checks
 	if (nodeArray[k].parent != 0) {
-		while (nodeArray[parentNmbr(k)].value > nodeArray[k].value) {	//changes the value between the new object and the parent, 
+		while ((nodeArray[parentNmbr(k)].value > nodeArray[k].value)&&(k != 1)) {	//changes the value between the new object and the parent, 
 			int aux;													//as long as the parent doesn't fit criteria
 			aux = nodeArray[parentNmbr(k)].value;
 			nodeArray[parentNmbr(k)].value = nodeArray[k].value;
 			nodeArray[k].value = aux;
+			k = parentNmbr(k);
 		}
 	}
+	
+	return 1;
 
 	
 }
