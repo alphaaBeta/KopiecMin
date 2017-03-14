20bdd95f1b3091b6e8518274557e3c34ef7785c3
Piotr Ogrodnik <piotrekogrodnik81@gmail.com>
2017-03-14 13:44:47 +0100

    REworks operator+

--- KopiecMin/heap.cpp
+++ KopiecMin/heap.cpp
@@ -10,9 +10,9 @@ int square(int a, int power) {
 	return ret;
 }
 
-inline int parentNmbr(int a) { return a / 2; }
-inline int leftChildNmbr(int a) { return a * 2; }
-inline int rightChildNmbr(int a) { return (a * 2) + 1; }
+inline int parentNmbr(int a) { return (a-1) / 2; }
+inline int leftChildNmbr(int a) { return a * 2 + 1; }
+inline int rightChildNmbr(int a) { return (a * 2) + 2; }
 
 
 //---------------------------------------
@@ -20,6 +20,8 @@ inline int rightChildNmbr(int a) { return (a * 2) + 1; }
 
 heap::heap() {
 	nodeArray = 0; //start of the array is pointing at NULL
+	cout << "fuu";
+
 }
 
 heap::~heap() {
@@ -28,65 +30,31 @@ heap::~heap() {
 }
 
 int heap::operator+(int val) {
-	numberOfNodes++;
 	int k = numberOfNodes;
-	//node *auxptr;	//creating new pointer pointing at a node
-	//auxptr = new node[k];	//creating a new array of nodes
-	if (k == 1) {
-		//memcpy(auxptr, nodeArray, sizeof(nodeArray));	//if that's not the first node we make,
-		//delete[] nodeArray;	//cpy the memory of the old array to the new, relasing the old one
-		nodeArray = new node[2];
+
+
+	if (k == 0) {
+		nodeArray = new node[1];
+		nodeArray->value = val;
+		nodeArray->parent = 0;
+		nodeArray->leftChild = 0;
+		nodeArray->rightChild = 0;
+		numberOfNodes++;
+		return 1;
 		
 	}
 	else {
 		nodeArray = static_cast<node*> (realloc(nodeArray, sizeof(node)*(k+1) ) ); //casting the returned void to node ptr
-
-	}
-	//nodeArray = auxptr;
-	if (k != 1) {
-		int i;
-		node *aux;
-		do {	//looks for the nearest empty node
-			if (nodeArray[i].leftChild == 0) {
-				aux = nodeArray[i].leftChild;
-				aux->value = val;
-				aux->parent = &nodeArray[i];
-				aux->leftChild = 0;
-				aux->rightChild = 0;
-				k = leftChildNmbr(i);
-			}
-			if (nodeArray[i].rightChild == 0) {
-				aux = nodeArray[i].rightChild;
-				aux->value = val;
-				aux->parent = &nodeArray[i];
-				aux->leftChild = 0;
-				aux->rightChild = 0;
-				k = rightChildNmbr(i);
-			}
-			i++;
-		} while (nodeArray[i].leftChild == 0 || nodeArray[i].rightChild == 0);
-	}
-
-	if (k == 1) { //for first node
-		nodeArray[k].parent = 0;
-		nodeArray[k].leftChild = 0;
-		nodeArray[k].rightChild = 0;
-	}
-	/*else {
+		nodeArray[k].value = val;
 		nodeArray[k].parent = &nodeArray[parentNmbr(k)];
 		nodeArray[k].leftChild = 0;
 		nodeArray[k].rightChild = 0;
-		if (!nodeArray[k].parent->leftChild) {
-			nodeArray[k].parent->leftChild = &nodeArray[k];
-		}
-		else {
-			nodeArray[k].parent->rightChild = &nodeArray[k];
-		}
-	}*/
-	//beginning checks
-	if (nodeArray[k].parent != 0) {
-		
+		swapCond(&nodeArray[k]); //nodeArray[1]
+
 	}
+	
+	
+	numberOfNodes++;
 	
 	return 1;
 
@@ -95,7 +63,8 @@ int heap::operator+(int val) {
 
 int heap::operator-(int val) {
 	node *aux;
-	while (aux) {
+	return 0;
+	while (1) {
 		aux = searchHeap(val);
 		if (!aux) { return 0; }
 		node *lc = aux->leftChild;
@@ -103,7 +72,7 @@ int heap::operator-(int val) {
 		if (rc || lc) {
 			if (lc && !rc) { //when there is no rightchild
 				aux->value = lc->value;
-				lc = 0;
+				lc = 0; // xD
 				numberOfNodes--;
 			}
 			else {
@@ -116,6 +85,7 @@ int heap::operator-(int val) {
 			}
 		}
 	}
+	
 }
 
 node *heap::searchHeap(int val) {
@@ -131,12 +101,28 @@ node *heap::searchHeap(int val) {
 }
 
 
-int heap::swapChild(node *a) {
-	while ((nodeArray[parentNmbr(k)].value > nodeArray[k].value) && (k != 1)) {	//changes the value between the new object and the parent, 
-		int aux;													//as long as the parent doesn't fit criteria
-		aux = nodeArray[parentNmbr(k)].value;
-		nodeArray[parentNmbr(k)].value = nodeArray[k].value;
-		nodeArray[k].value = aux;
-		k = parentNmbr(k);
+int heap::swapCond(node *a) {
+	while (a->parent && a->parent->value > a->value) {	//changes the value between the new object and the parent, 
+		int aux = a->value;													//as long as the parent doesn't fit criteria
+		a->value = a->parent->value;
+		a->parent->value = aux;
+		a = a->parent;
 	}
+	//if(!a->leftChild && ) {
+		while ((a->leftChild && a->leftChild->value < a->value) ||
+				(a->rightChild && a->rightChild->value < a->value)) {
+
+			if (a->leftChild->value < a->rightChild->value) {
+				int aux = a->rightChild->value;		//changes both child nodes so that the smaller one is to the right
+				a->rightChild->value = a->leftChild->value;
+				a->leftChild->value = aux;
+			}
+			int aux = a->value;
+			a->value = a->rightChild->value;
+			a->rightChild->value = aux;
+			a = a->rightChild;
+		}
+	//}
+	//catch (...) {}
+	return 1;
 }
