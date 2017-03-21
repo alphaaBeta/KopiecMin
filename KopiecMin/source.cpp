4d84166a3476f1190128a63d313000b97bb4e3d0
Piotr Ogrodnik <piotrekogrodnik81@gmail.com>
2017-03-21 13:56:09 +0100

    Finishing.]

--- KopiecMin/source.cpp
+++ KopiecMin/source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-using namespace std;
+//using namespace std;
 
 
 //----------------------------------
@@ -8,48 +8,11 @@ using namespace std;
 #include "HeapTester.h"
 
 
-//bool addTest() {
-//	Heap test1; 
-//	test1 += 2;
-//	test1 += 5;
-//	return (test1.contains(2) && test1.contains(5) && test1.size() == 2);
-//}
 
 int main() {
-	/*
-	Heap test;	// {1,2,5,5,7}
-	test = test + 2;
-	test = test + 5;
-	test = test + 5;
-	test = test + 7;
-	test = test + 1;
-	cout << test;
-
-	Heap test2;	// {2,2,3,4,7}
-	test2 += 2;
-	test2 += 7;
-	test2 += 3;
-	test2 += 4;
-	test2 += 2;
-	cout << test2;
-
-	test2 = test;
-	cout << test2;
-
-	test += test2;
-
-	cout << test;
-
-	test -= test2;
-	//test.Draw();
-	cout << test;
-
-	cout << test[1];
-	*/
 	HeapTester test1;
-	cout << test1.DoTests();
-
-	//int *i = new int[1000];
+	cout << test1.DoTests(); 
+	
 	getchar();
 	return 0;
 }
